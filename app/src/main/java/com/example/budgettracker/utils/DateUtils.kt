5f53ac0fa91feece8f2a9d846ac418f0c04fa4f5
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-13 00:47:36 +0700

    Create DateUtils.kt

---
+++ app/src/main/java/com/example/budgettracker/utils/DateUtils.kt
@@ -0,0 +1,18 @@
+package com.example.budgettracker.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+object DateUtils {
+    private val format = SimpleDateFormat("dd.MM.yyyy", Locale.getDefault())
+
+    fun timestampToString(timestamp: Long): String = format.format(Date(timestamp))
+    fun getStartOfMonth(): Long {
+        val calendar = Calendar.getInstance()
+        calendar.set(Calendar.DAY_OF_MONTH, 1)
+        calendar.set(Calendar.HOUR_OF_DAY, 0)
+        calendar.set(Calendar.MINUTE, 0)
+        calendar.set(Calendar.SECOND, 0)
+        return calendar.timeInMillis
+    }
+}
