45f0d52f15a41ace41e69b45cd6b90d2809e95ce
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-18 08:52:13 +0700

    Update AboutFragment.kt

--- app/src/main/java/com/example/budgettracker/AboutFragment.kt
+++ app/src/main/java/com/example/budgettracker/AboutFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_about, container, false)
-        view.findViewById<TextView>(R.id.tvPlaceholder).text = "О нас: приложение разработано ..."
-        return view
+        return inflater.inflate(R.layout.fragment_about, container, false)
     }
 }
