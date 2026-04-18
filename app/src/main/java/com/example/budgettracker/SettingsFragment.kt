8841c5901e18c8b9ddf7c57299c70712600488ed
tiktokfg957 <tiktokfg957@gmail.com>
2026-04-18 08:52:02 +0700

    Update SettingsFragment.kt

--- app/src/main/java/com/example/budgettracker/SettingsFragment.kt
+++ app/src/main/java/com/example/budgettracker/SettingsFragment.kt
@@ -4,13 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 
 class SettingsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_settings, container, false)
-        view.findViewById<TextView>(R.id.tvPlaceholder).text = "Настройки будут здесь"
-        return view
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
